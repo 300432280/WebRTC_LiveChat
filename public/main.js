5c015900ff0dd7749c0de294c3902ba5b7a0dccd
Ubuntu <ubuntu@ip-172-31-31-237.ec2.internal>
2020-01-22 04:05:01 +0000

    add more

--- public/main.js
+++ public/main.js
@@ -1,1 +1,11 @@
 var Peer = require('simple-peer')
+var socket = io()
+const video = document.querySelector('video')
+var client = {}
+
+// get stream
+navigator.mediaDevices.getUserMedia({video:true, audio:true})
+.then(stream => {
+    
+})
+.catch(err => document.write(err))
